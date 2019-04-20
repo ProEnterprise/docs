9d176ad1a494777acd2cedd60f2393acf53000e0
Adam Dawoodjee <adam.daveed@gmail.com>
2019-04-20 21:26:21 +0200

    Update setup.py
    updated for compatibility with pip 19.0.3

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in docs/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('docs/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
 
-requirements = parse_requirements("requirements.txt", session="")
+with open('docs/__init__.py', 'rb') as f:
+        version = str(ast.literal_eval(_version_re.search(
+                f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='docs',
 	version=version,
-	description='Pro Enterprise ERP Documentation.',
-	author='Pro Enterprise Limited',
-	author_email='enterprisezambia@gmail.com',
+	description='Proenterprise ERP Documentation.',
+	author='Proenterprise Limited',
+	author_email='info@erp.co.zm',
 	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+        zip_safe=False,
+        include_package_data=True,
+        install_requires=install_requires
 )
